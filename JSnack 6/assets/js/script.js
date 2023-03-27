2fa3460038046297eba3434c16863ece43c6ef08
raffaele-catalano <raf.catalano@gmail.com>
2023-03-27 17:59:55 +0200

    added solution w/ "while" cycle

---
+++ JSnack 6/assets/js/script.js
@@ -0,0 +1,26 @@
+/*
+
+1- predisporre un array vuoto
+2- chiedere all'utente di inserire un numero
+3- in base al numero inserito dall'utente stampare in pagina il cubo di ogni singolo numero che compone l'intervallo numerico da 0 fino al numero inserito
+4- utilizzare a tale scopo un ciclo "while"
+
+
+
+
+*/
+
+const arrayCubi = [];
+
+let numeroInserito = prompt('Inserisci un numero');
+
+let counterNum = 0;
+
+while (counterNum <= numeroInserito) {
+    // let cuboProdotto = Math.cube(counterNum)
+    let cuboProdotto = (counterNum * counterNum * counterNum);
+    arrayCubi.push(cuboProdotto);
+
+    document.getElementById('output').innerHTML = arrayCubi;
+    counterNum++
+}
